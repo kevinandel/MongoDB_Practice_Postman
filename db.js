bd90be6e74866698ae776359277fdcbcc61440a0
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-09-27 12:23:14 +0530

    Practiced MongoDB and Node combined with Postman API

---
+++ db.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb')
+
+let dbConnection;
+
+module.exports = {
+  connectToDb: (cb) => {
+    MongoClient.connect('mongodb://localhost:27017/bookstore').then((client) => {
+      dbConnection = client.db()
+      return cb();
+    }).catch(error => {
+      console.log(error)
+      return cb(error)
+    })
+  },
+  getDb: () => dbConnection
+}
